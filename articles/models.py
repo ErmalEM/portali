7273138bee438fdde578381d06163ee51a06d205
ErmalEM <em38943@ubt-uni.net>
2023-02-09 10:47:52 +0100

    created article listview detailview updateview deleteview and create view

--- articles/models.py
+++ articles/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    author_a = models.ForeignKey(
+    author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
     )
